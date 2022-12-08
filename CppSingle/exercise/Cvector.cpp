60e8d1b8414b6bb9572de66d031c039f126f137d
Miaocf <miaocf@shanghaitech.edu.cn>
2022-12-09 01:20:36 +0800

    Chore: modify vscode config from C to C plus pluss

---
+++ CppSingle/exercise/Cvector.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+/* 注: 这里目录配置为在 exercise 下面进行编译。 生成的可执行文件放在 exercise/bin/xxx.exe 目录下
+ * Build        : Ctrl + Shift + B
+ * Build + Run  : F4
+ * Debug        : F5 
+ */ 
+
+int main() 
+{
+    string str = "Vector parctic.";
+    cout << str << endl;
+    cin.get();
+    return 0;
+}  
